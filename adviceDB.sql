b76010fbd8804cfa53be9ab71b8b83e7298c73a6
RussMasu <46938458+RussMasu@users.noreply.github.com>
2019-03-27 10:52:05 -1000

    Update adviceDB.sql

--- adviceDB.sql
+++ adviceDB.sql
@@ -1,9 +1,15 @@
 CREATE DATABASE adviceDB;
 USE adviceDB;
 CREATE TABLE Person (
-Username varchar(20),
+Username varchar(20) NOT NULL,
 PName varchar(100),
-FAS_ID int(1),
 Graduation_Date date,
-Contact_Info varchar(1000)
+Contact_Info varchar(1000),
+FAS_Val ENUM('Faculty','Alumni','Student'),
+PRIMARY KEY (Username)
+);
+CREATE TABLE Person_MajorField(
+Major_Field varchar(100),
+Username varchar(100),
+FOREIGN KEY (Username) REFERENCES Person(Username)
 );
