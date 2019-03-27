0a2787f6a440a9ac2f74d10bb7afe8f7a56fe810
RussMasu <46938458+RussMasu@users.noreply.github.com>
2019-03-27 13:56:58 -1000

    Update adviceDB.sql

--- adviceDB.sql
+++ adviceDB.sql
@@ -13,3 +13,15 @@ Major_Field varchar(100),
 Username varchar(100),
 FOREIGN KEY (Username) REFERENCES Person(Username)
 );
+CREATE TABLE Club(
+Club_Name varchar(100) NOT NULL,
+Site_link varchar(1000),
+PRIMARY KEY (Club_Name)
+)
+CREATE TABLE Club_Person(
+Club_Name varchar(100) NOT NULL,
+Username varchar(20) NOT NULL,
+FOREIGN KEY (Club_Name) REFERENCES Club(Club_Name),
+FOREIGN KEY (Username) REFERENCES Person(Username),
+PRIMARY KEY (Club_Name,Username)
+)
