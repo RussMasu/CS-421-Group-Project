ca33094276f18ce3cfb0761284d14690cc75d815
RussMasu <46938458+RussMasu@users.noreply.github.com>
2019-03-28 17:21:14 -1000

    Update adviceDB.sql

--- adviceDB.sql
+++ adviceDB.sql
@@ -29,19 +29,25 @@ PRIMARY KEY (Club_Name,Username)
 CREATE TABLE School(
 School_Name varchar(100) NOT NULL,
 Address varchar(100) NOT NULL,
-Accreditation_Val ENUM('1','2'),
 Enrollment_Val ENUM('small','medium','large'),
 PRIMARY KEY(School_Name)
 );
 CREATE TABLE School_Person(
 School_Name varchar(100) NOT NULL,
 Username varchar(20) NOT NULL,
-Degree_Val ENUM('Biology','Computer Science','Math'),
 Start_Date date NOT NULL,
 End_Date date,
 FOREIGN KEY(School_Name) REFERENCES School(School_Name),
 FOREIGN KEY(Username) REFERENCES Person(Username),
 PRIMARY KEY(School_Name,Username)
+);
+CREATE TABLE SchoolPerson_Degree(
+School_Name varchar(100) NOT NULL,
+Username varchar(20) NOT NULL,
+Degree varchar(40) NOT NULL,
+FOREIGN KEY(School_Name) REFERENCES School(School_Name),
+FOREIGN KEY(Username) REFERENCES Person(Username),
+PRIMARY KEY(School_Name,Username,Degree)
 );
 CREATE TABLE Advice(
 Advice_ID int(12) NOT NULL,
@@ -61,4 +67,44 @@ Resources varchar(100) NOT NULL,
 Advice_ID int(12),
 FOREIGN KEY(Advice_ID) REFERENCES Advice(Advice_ID),
 PRIMARY KEY(Resources,Advice_ID)
+);
+CREATE TABLE Job(
+Job_ID TINYINT NOT NULL,
+Job_Title varchar(100) NOT NULL,
+Pay DECIMAL(11,2),
+Salaried Boolean,
+Interview_Process varchar(10000),
+Benefits varchar(1000),
+PRIMARY KEY (Job_ID)
+);
+CREATE TABLE Job_Person(
+Job_ID TINYINT NOT NULL,
+Username varchar(20) NOT NULL,
+Start_Date date NOT NULL,
+End_Date date,
+PRIMARY KEY (Job_ID, Username),
+FOREIGN KEY (Job_ID) REFERENCES Job(Job_ID),
+FOREIGN KEY (Username) REFERENCES Person(Username)
+);
+CREATE TABLE Company(
+Company_Name varchar(100) NOT NULL,
+Num_Emp TINYINT,
+Address varchar(40) NOT NULL,
+PRIMARY KEY (Company_Name)
+);
+CREATE TABLE Company_Job(
+Company_Name varchar(100) NOT NULL,
+Job_ID TINYINT NOT NULL,
+FOREIGN KEY (Company_Name) REFERENCES Company(Company_Name),
+FOREIGN KEY (Job_ID) REFERENCES Job(Job_ID),
+PRIMARY KEY (Company_Name, Job_ID)
+);
+CREATE TABLE Company_Industry(
+Company_Name varchar(20) NOT NULL,
+Industry ENUM('Basic Industries','Capital Goods','Consumer Durables',
+'Consumer Non-Durables','Consumer Services','Energy','Finance',
+'Healthcare','Public Utilities','Technology', 'Transporation',
+'Miscellaenous') NOT NULL,
+FOREIGN KEY (Company_Name) REFERENCES Company(Company_Name),
+PRIMARY KEY (Company_Name, Industry)
 );
