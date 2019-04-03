d8adf04d44f4bacf1feb6d39556d8eb817894cf9
RussMasu <46938458+RussMasu@users.noreply.github.com>
2019-04-02 15:10:58 -1000

    Update adviceDB.sql

--- adviceDB.sql
+++ adviceDB.sql
@@ -135,14 +135,18 @@ VALUES('Lane Burton','Lane N. Burton','Faculty','2020-03-02','555-465-7125, 55 R
 INSERT INTO Person_MajorField(Major_Field,Username)
 VALUES('Computer Science','ZacP');
 INSERT INTO Person_MajorField(Major_Field,Username)
-VALUES('Computer Science','rbot15');
+VALUES('Physics','rbot15');
 INSERT INTO Person_MajorField(Major_Field,Username)
-VALUES('Computer Science,Psychics','SpinyPhish');
+VALUES('Computer Science','SpinyPhish');
 INSERT INTO Person_MajorField(Major_Field,Username)
-VALUES('Psychics','RRandal');
+VALUES('Psysics','SpinyPhish');
+INSERT INTO Person_MajorField(Major_Field,Username)
+VALUES('Psysics','RRandal');
 INSERT INTO Person_MajorField(Major_Field,Username)
 VALUES('Computer Science','Lane Burton');
 INSERT INTO Person_MajorField(Major_Field,Username)
 VALUES('Computer Science','Sven47');
 INSERT INTO Person_MajorField(Major_Field,Username)
-VALUES('Computer Science, Biology','Shannon Lawsons');
+VALUES('Computer Science','Shannon Lawsons');
+INSERT INTO Person_MajorField(Major_Field,Username)
+VALUES('Biology','Shannon Lawsons');
